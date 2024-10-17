f714bf0d905e3db24c8bbfe88a66295ea66a5ee2
Donghyun Kim <hyundiego@hanyang.ac.kr>
2024-10-17 19:07:11 +0900

    Update 785A.cpp

--- string/785A.cpp
+++ string/785A.cpp
@@ -5,33 +5,33 @@ using namespace std;
 
 int main()
 {
-	  string s0="Tetrahedron";
-    string s1="Cube";
-    string s2="Octahedron";
-    string s3="Dodecahedron";
-    string s4="Icosahedron";
+	string s0="Tetrahedron";
+	string s1="Cube";
+	string s2="Octahedron";
+	string s3="Dodecahedron";
+	string s4="Icosahedron";
 	
-    int t;
-	  int ans=0;
-
-    cin>>t;
-    while(t--)
-    {
-        string m="";
+	int t;
+	int ans=0;
 	
-        cin>>m;
-        if(!m.compare(s0))
-            ans+=4;
-        if(!m.compare(s1))
-            ans+=6;
-        if(!m.compare(s2))
-            ans+=8;
-        if(!m.compare(s3))
-            ans+=12;
-        if(!m.compare(s4))
-            ans+=20;
-    }
-    cout<<ans<<'\n';
-
-    return 0;
+	cin>>t;
+	while(t--)
+	{
+		stirng m="";
+		
+		cin>>m;
+		if(!m.compare(s0))
+			ans+=4;
+		if(!m.compare(s1))
+			ans+=6;
+		if(!m.compare(s2))
+			ans+=8;
+		if(!m.compare(s3))
+			ans+=12;
+		if(!m.compare(s4))
+			ans+=20;
+	}
+	cout<<ans<<'\n';
+	
+	return 0;
 }
